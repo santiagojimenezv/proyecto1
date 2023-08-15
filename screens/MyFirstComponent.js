eeb40ea74c967cea3a8583d7d7946d97c0418224
santiagojimenezv <s.jimenezv10@gmail.com>
2023-08-15 11:16:48 -0500

    My First Component created

--- screens/MyFirstComponent.js
+++ screens/MyFirstComponent.js
@@ -1,11 +1,12 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { SafeAreaView, Text } from 'react-native'
 
 const MyFirstComponent = () => {
-    const [userName, setUserName] = useState('')
+
+  const [userName, setUserName] = useState('')
   return (
     <SafeAreaView>
-      <Text>My First Component</Text>
+      <Text>My First Component By Santiago Jimenez</Text>
     </SafeAreaView>
   )
 }
